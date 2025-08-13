c3ab90dacc532d706deb3022a4cd2b0686e0bb46
wonge1 <e.wong1@lancaster.ac.uk>
2025-08-13 23:45:30 +0100

    base image

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,16 @@ import BaseLayout from "@/components/base-layout";
 
 export default function Home() {
   return (
-      <BaseLayout>
-        <div>
-          <div id="section1" className="bg-gradient-to-b h-screen">
-  
-          </div>
-          <div id="section2" className="h-96 bg-amber-300">
-  
-          </div>
+      <BaseLayout><div>
+        <div id="landing" className="h-screen
+                                      bg-[url(/portfolio/images/loch.jpg)] bg-cover bg-center">
         </div>
+        <div id="experience" className="h-96 bg-amber-900">
+        </div>
+        <div id="section2" className="h-96 bg-amber-300">
+
+        </div>
+      </div>
       </BaseLayout>
   );
 }
